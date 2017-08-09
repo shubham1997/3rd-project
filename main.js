bdc8e2ea92375b91aed2227adef13eb627dc1db8
shubhambhagwal1997@gmail.com <shubhambhagwal1997@gmail.com>
2017-08-09 20:21:04 +0530

    with different images

--- main.js
+++ main.js
@@ -33,40 +33,44 @@ foodieApp.controller('mainController',function($scope) {
 	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
 },
                           {
-	name: 'happy',
-	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
-	location: 'Connaught Place',
-	category: 'Casual Dining, Bar',
-	vote: '4.2',
-	cuisines: 'Modern Indian',
-	cost: '2200',
-	hours: '12 Noon to 1 AM (Mon-Sun)',
-	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+	name: 'Misso Hungry',
+	address: 'Booth 41, Sector 34, Chandigarh',
+	location: 'Chandigrah',
+	category: '',
+	vote: '4.6',
+	cuisines: 'Rolls, Fast Food',
+	cost: '1700',
+	hours: '1 PM to 10:30 PM(Mon-Sun)',
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnUBTcmsQb7w3YlL1Nwt08t8xoxyMBpvKVkkQBkW4WAXhxsu-k'
 },
                           {
 	name: 'dominoze',
-	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+	address: 'T 1-107, 1st Floor, One Horizon Centre, Golf Course Road, Gurgaon',
 	location: 'Connaught Place',
 	category: 'Casual Dining, Bar',
-	vote: '4.2',
-	cuisines: 'Modern Indian',
-	cost: '2200',
-	hours: '12 Noon to 1 AM (Mon-Sun)',
-	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+	vote: '4.0',
+	cuisines: 'Japanese, Chinese, Thai',
+	cost: '1400',
+	hours: '  12 Noon to 11 PM (Mon-Sun)',
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0TN-R0OiyveXxPe2PQkcXsYOXdOm_1IwoLmt_B-FsaIylQ0is'
 },
                           {
-	name: 'hahahahaa',
-	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
-	location: 'Connaught Place',
-	category: 'Casual Dining, Bar',
-	vote: '4.2',
-	cuisines: 'Modern Indian',
-	cost: '2200',
-	hours: '12 Noon to 1 AM (Mon-Sun)',
-	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+	name: 'Caffe Tonino',
+	address: '1st Floor, One Horizon Center, Golf Course Road, Gurgaon',
+	location: 'One Horizon Center, Golf Course',
+	category: 'Casual Dining,Caffe',
+	vote: '3.9',
+	cuisines: 'Italian, Pizza, Cafe',
+	cost: '1000',
+	hours: '8:30Am to 9PM (Mon-Sun)',
+	image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeLzjuPjm8BslLlghSNxRKoCcgW9X_c_oAPr2cBUrUb4fo9yIPMQ',
 }];
 })
 
+                          
+                          
+                          
+                          
 foodieApp.controller('loginController',function($scope,$location) {
 	$scope.goToHome = function() {
 		// console.log('Do Something')
@@ -77,22 +81,8 @@ foodieApp.controller('restaurantController',function($scope,$routeParams,$http) 
 	$scope.restaurantId = $routeParams.id;
 	$scope.ingredients = [];
 	var restaurants =
-    [{name: 'Farzi Cafe',
-	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
-	location: 'Connaught Place',
-	category: 'Casual Dining, Bar',
-	vote: '4.2',
-	cuisines: 'Modern Indian',
-	cost: '2200',
-	hours: '12 Noon to 1 AM (Mon-Sun)',
-      bestDish: {
-	name: 'Corn Pizza',
-	image: 'http://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG'
-},
-	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
-},
-                          {
-	name: 'happy',
+     [{
+	name: 'Farzi Cafe',
 	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
 	location: 'Connaught Place',
 	category: 'Casual Dining, Bar',
@@ -104,24 +94,56 @@ foodieApp.controller('restaurantController',function($scope,$routeParams,$http) 
 	name: 'Corn Pizza',
 	image: 'http://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG'
     },
+         
 	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
 },
                           {
+	name: 'Misso Hungry',
+	address: 'Booth 41, Sector 34, Chandigarh',
+	location: 'Chandigrah',
+	category: '',
+	vote: '4.6',
+	cuisines: 'Rolls, Fast Food',
+	cost: '1700',
+	hours: '1 PM to 10:30 PM(Mon-Sun)',
+    bestDish: {
+	name: 'vegetable Pizza',
+	image: 'https://images.pexels.com/photos/2232/vegetables-italian-pizza-restaurant.jpg?h=350&auto=compress&cs=tinysrgb'
+    },
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnUBTcmsQb7w3YlL1Nwt08t8xoxyMBpvKVkkQBkW4WAXhxsu-k'
+},
+                          {
 	name: 'dominoze',
-	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+	address: 'T 1-107, 1st Floor, One Horizon Centre, Golf Course Road, Gurgaon',
 	location: 'Connaught Place',
 	category: 'Casual Dining, Bar',
-	vote: '4.2',
-	cuisines: 'Modern Indian',
-                              
-	cost: '2200',
-	hours: '12 Noon to 1 AM (Mon-Sun)',
-                              bestDish: {
-	name: 'Corn Pizza',
-	image: 'http://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG'
+	vote: '4.0',
+	cuisines: 'Japanese, Chinese, Thai',
+	cost: '1400',
+	hours: '  12 Noon to 11 PM (Mon-Sun)',
+    bestDish: {
+	name: 'Pasta',
+	image: 'https://images.pexels.com/photos/64208/pexels-photo-64208.jpeg?h=350&auto=compress&cs=tinysrgb'
+    },
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0TN-R0OiyveXxPe2PQkcXsYOXdOm_1IwoLmt_B-FsaIylQ0is'
 },
-	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
-}]
+                          {
+	name: 'Caffe Tonino',
+	address: '1st Floor, One Horizon Center, Golf Course Road, Gurgaon',
+	location: 'One Horizon Center, Golf Course',
+	category: 'Casual Dining,Caffe',
+	vote: '3.9',
+	cuisines: 'Italian, Pizza, Cafe',
+	cost: '1000',
+	hours: '8:30Am to 9PM (Mon-Sun)',
+    bestDish: {
+	name: 'Corn Pizza',
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnUBTcmsQb7w3YlL1Nwt08t8xoxyMBpvKVkkQBkW4WAXhxsu-k'
+    },
+	image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeLzjuPjm8BslLlghSNxRKoCcgW9X_c_oAPr2cBUrUb4fo9yIPMQ',
+}];
+    
+    
 	$scope.restaurant = restaurants[$routeParams.id - 1];
    $scope.getIngredients = function(url) {
 	var data = '{"inputs":[{"data":{"image":{"url":"' + url + '"}}}]}'
@@ -145,3 +167,4 @@ foodieApp.controller('restaurantController',function($scope,$routeParams,$http) 
         	console.log(xhr);
         }
 	})
+                       
