338db77bc6141f183ec889c611dd27ad254b2f35
shubham1997 <shubhambhagwal1997@gmail.com>
2017-08-09 13:02:16 +0530

    class 7

--- main.js
+++ main.js
@@ -14,6 +14,10 @@ foodieApp.config(function ($routeProvider) {
 		templateUrl: 'pages/main.html',
 		controller: 'mainController'
 	})
+	.when('/restaurant/:id', {
+		templateUrl: 'pages/restaurant.html',
+		controller: 'restaurantController'
+	})
 })
 
 foodieApp.controller('mainController',function($scope) {
@@ -29,7 +33,7 @@ foodieApp.controller('mainController',function($scope) {
 	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
 },
                           {
-	name: 'Farzi Cafe',
+	name: 'happy',
 	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
 	location: 'Connaught Place',
 	category: 'Casual Dining, Bar',
@@ -40,7 +44,7 @@ foodieApp.controller('mainController',function($scope) {
 	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
 },
                           {
-	name: 'Farzi Cafe',
+	name: 'dominoze',
 	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
 	location: 'Connaught Place',
 	category: 'Casual Dining, Bar',
@@ -51,7 +55,7 @@ foodieApp.controller('mainController',function($scope) {
 	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
 },
                           {
-	name: 'Farzi Cafe',
+	name: 'hahahahaa',
 	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
 	location: 'Connaught Place',
 	category: 'Casual Dining, Bar',
@@ -63,5 +67,81 @@ foodieApp.controller('mainController',function($scope) {
 }];
 })
 
-foodieApp.controller('loginController',function($scope) {
+foodieApp.controller('loginController',function($scope,$location) {
+	$scope.goToHome = function() {
+		// console.log('Do Something')
+		$location.url('home')
+	}
 })
+foodieApp.controller('restaurantController',function($scope,$routeParams,$http) {
+	$scope.restaurantId = $routeParams.id;
+	$scope.ingredients = [];
+	var restaurants =
+    [{name: 'Farzi Cafe',
+	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+	location: 'Connaught Place',
+	category: 'Casual Dining, Bar',
+	vote: '4.2',
+	cuisines: 'Modern Indian',
+	cost: '2200',
+	hours: '12 Noon to 1 AM (Mon-Sun)',
+      bestDish: {
+	name: 'Corn Pizza',
+	image: 'http://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG'
+},
+	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+},
+                          {
+	name: 'happy',
+	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+	location: 'Connaught Place',
+	category: 'Casual Dining, Bar',
+	vote: '4.2',
+	cuisines: 'Modern Indian',
+	cost: '2200',
+	hours: '12 Noon to 1 AM (Mon-Sun)',
+    bestDish: {
+	name: 'Corn Pizza',
+	image: 'http://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG'
+    },
+	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+},
+                          {
+	name: 'dominoze',
+	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+	location: 'Connaught Place',
+	category: 'Casual Dining, Bar',
+	vote: '4.2',
+	cuisines: 'Modern Indian',
+                              
+	cost: '2200',
+	hours: '12 Noon to 1 AM (Mon-Sun)',
+                              bestDish: {
+	name: 'Corn Pizza',
+	image: 'http://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG'
+},
+	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+}]
+	$scope.restaurant = restaurants[$routeParams.id - 1];
+   $scope.getIngredients = function(url) {
+	var data = '{"inputs":[{"data":{"image":{"url":"' + url + '"}}}]}'
+	$http({
+		'method': 'POST',
+		'url': 'https://api.clarifai.com/v2/models/bd367be194cf45149e75f01d59f77ba7/outputs',
+		'headers': {
+			'Authorization': 'Key cb16853fb3d748d4999d863c4512cefa',
+			'Content-Type': 'application/json'
+		},
+		'data': data
+	}).then(function (response) {
+				var ingredients = response.data.outputs[0].data.concepts;
+				for (var i =0;i < ingredients.length;i++) {
+				$scope.ingredients.push(ingredients[i].name);
+				}
+  			})
+    		// $('.ingredients').html(list);
+    		console.log(list);
+        }, function (xhr) {
+        	console.log(xhr);
+        }
+	})
