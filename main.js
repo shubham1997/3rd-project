c16de2a4274677d7952a0bf539ac3c72a900a0e8
shubham1997 <shubhambhagwal1997@gmail.com>
2017-08-15 17:20:34 +0530

    variant problem

--- main.js
+++ main.js
@@ -4,6 +4,11 @@
 //        $('.welcome-message').text(message);
 //    });
 var foodieApp = angular.module('foodieApp',['ngRoute']);
+console.log(foodieApp);
+var list;
+
+//function for routing
+
 foodieApp.config(function ($routeProvider) {
 	$routeProvider
 	.when('/',{
@@ -19,6 +24,8 @@ foodieApp.config(function ($routeProvider) {
 		controller: 'restaurantController'
 	})
 })
+
+//function for maincontroller
 
 foodieApp.controller('mainController',function($scope) {
 	$scope.restaurants = [{
@@ -64,11 +71,101 @@ foodieApp.controller('mainController',function($scope) {
 	cost: '1000',
 	hours: '8:30Am to 9PM (Mon-Sun)',
 	image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeLzjuPjm8BslLlghSNxRKoCcgW9X_c_oAPr2cBUrUb4fo9yIPMQ',
-}];
+},
+    {
+		name: 'Pirates',
+		address: '38/39, Level 10, Block F , Inner Circle, Corol bagh',
+		location: 'Connaught Place',
+		category: 'Bakery',
+		vote: '4.4',
+		cuisines: 'Desert',
+		cost: '200',
+		hours: '12 Noon to 10 PM (Mon-Sun)',
+		image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+		
+		,                     
+ }];
+
+    console.log($scope.restaurants[0].name);
+$scope.change = function(){
+$scope.restaurants.push({
+	name: 'dominoze',
+	address: 'T 1-107, 1st Floor, One Horizon Centre, Golf Course Road, Gurgaon',
+	location: 'Connaught Place',
+	category: 'Casual Dining, Bar',
+	vote: '4.0',
+	cuisines: 'Japanese, Chinese, Thai',
+	cost: '1400',
+	hours: '  12 Noon to 11 PM (Mon-Sun)',
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0TN-R0OiyveXxPe2PQkcXsYOXdOm_1IwoLmt_B-FsaIylQ0is'
+});
+
+console.log($scope);
+console.log($scope.$parent.restaurants);
+
+}
+
 })
 
+
+// Controller for todolist page
+foodieApp.controller("todo", function($scope) {
+			$scope.list = list;
+			$scope.x=1;
+			console.log(list);
+			$scope.grocery = [];
+			//I need to create an array of object because we will need this for keeping track of which has been bought and which not
+			for (var i = 0; i < list.length; i++) {
+				var a = {
+					id: i,
+					name: list[i],
+					buy: false
+				};
+				$scope.grocery.push(a);
+			}
+			console.log($scope);
+			$scope.bought = [];
+			$scope.nbought = [];
+
+			$scope.toggle = function(num) {
+				// we need to change value dependin
+				$scope.grocery[num].buy = !$scope.grocery[num].buy;
+				$scope.$parent.grocery[num].buy = !$scope.$parent.grocery[num].buy
+
+			}
+			$scope.one =function(){
+
+				$scope.x=1;
+
+			}
+			$scope.two =function(){
+
+				$scope.x=2;
+
+			}
+			$scope.three =function(){
+
+				$scope.x=3;
+
+			}
+			$scope.filter = function() {
+$scope.bought=[];
+$scope.nbought=[];
+				for (var i = 0; i < $scope.grocery.length; i++) {
+					if ($scope.grocery[i].buy) {
+						$scope.bought.push($scope.grocery[i]);
+					} else {
+						$scope.nbought.push($scope.grocery[i]);
+					}
+				}
+
+				console.log($scope.bought);
+			}
+
+
+})
                           
-                          
+//        function for logincontroller                  
                           
                           
 foodieApp.controller('loginController',function($scope,$location) {
@@ -77,6 +174,10 @@ foodieApp.controller('loginController',function($scope,$location) {
 		$location.url('home')
 	}
 })
+
+//function for restaurantcontroller
+
+
 foodieApp.controller('restaurantController',function($scope,$routeParams,$http) {
 	$scope.restaurantId = $routeParams.id;
 	$scope.ingredients = [];
@@ -91,7 +192,7 @@ foodieApp.controller('restaurantController',function($scope,$routeParams,$http) 
 	cost: '2200',
 	hours: '12 Noon to 1 AM (Mon-Sun)',
     bestDish: {
-	name: 'Corn Pizza',
+	name: 'three bean salad',
 	image: 'http://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG'
     },
          
@@ -144,6 +245,14 @@ foodieApp.controller('restaurantController',function($scope,$routeParams,$http) 
 }];
     
     
+//    for creating ingredients list 
+        
+	$scope.x = 0;
+				$scope.toggle=function(){
+   			$scope.x = 1-$scope.x;
+				console.log($scope.x);
+				}
+    
 	$scope.restaurant = restaurants[$routeParams.id - 1];
    $scope.getIngredients = function(url) {
 	var data = '{"inputs":[{"data":{"image":{"url":"' + url + '"}}}]}'
@@ -166,5 +275,5 @@ foodieApp.controller('restaurantController',function($scope,$routeParams,$http) 
         }, function (xhr) {
         	console.log(xhr);
         }
-	})
+})
                        
